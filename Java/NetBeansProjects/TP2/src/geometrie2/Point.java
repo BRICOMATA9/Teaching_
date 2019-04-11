6a5636369a5d593164bd24745a66a496936b307b
aghiles <githubfortyuds@gmail.com>
2019-04-11 05:26:27 +0200

    commit

--- Java/NetBeansProjects/TP2/src/geometrie2/Point.java
+++ Java/NetBeansProjects/TP2/src/geometrie2/Point.java
@@ -4,23 +4,27 @@ public class Point {
 
     private int x;
     private int y;
+    private String nom;
+    
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
+    
+    public Point(String nom) {
+        new Point();
+        this.nom=nom;
+    }
 
     public Point(Point p) {
+        
         this.x = p.x;
         this.y = p.y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 
     public void setX(int x) {
@@ -31,12 +35,28 @@ public class Point {
         this.y = y;
     }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
     public String toString() {
         return ("(" + x + "," + y + ")");
     }
 
     public void deplacer(int dx, int dy) {
-        x = x + dx;
-        y = y + dy;
+        x = this.x + dx;
+        y = this.y + dy;
     }
 }
