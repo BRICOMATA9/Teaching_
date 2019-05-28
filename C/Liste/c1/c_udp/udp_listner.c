6c63673038970be077598c84f7473b6ed30094ff
aghiles <githubfortyuds@gmail.com>
2019-05-28 19:10:43 +0200

    commit

--- C/Liste/c1/c_udp/udp_listner.c
+++ C/Liste/c1/c_udp/udp_listner.c
@@ -1,97 +1,47 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define MAX_PORT 5
-#define PORT_ARRAY_SIZE (MAX_PORT+1)
-#define MAX_MSG 80
-#define MSG_ARRAY_SIZE (MAX_MSG+1)
-#define BACKLOG 5
-// Utilisation d'une constante x dans la définition
-// du format de saisie
 #define str(x) # x
-#define xstr(x) str(x)
 
-int main()
-{
+int main(){
   int listenSocket, connectSocket, status, i;
   unsigned short int msgLength;
   struct addrinfo hints, *servinfo;
   struct sockaddr_in clientAddress;
   socklen_t clientAddressLength = sizeof clientAddress;
-  char msg[MSG_ARRAY_SIZE], listenPort[PORT_ARRAY_SIZE];
+  char msg[81], listenPort[6];
 
   memset(listenPort, 0, sizeof listenPort);  // Mise à zéro du tampon
-  puts("Entrez le numéro de port utilisé en écoute (entre 1500 et 65000) : ");
-  scanf("%"xstr(MAX_PORT)"s", listenPort);
-
   memset(&hints, 0, sizeof hints);
+
+  puts("Port Serveur: [1500-65000]");
+  scanf("%"str(5)"s", listenPort);
   hints.ai_family = AF_INET;       // IPv4
   hints.ai_socktype = SOCK_STREAM; // TCP
   hints.ai_flags = AI_PASSIVE;     // Toutes les adresses disponibles
 
-  if ((status = getaddrinfo(NULL, listenPort, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-    exit(EXIT_FAILURE);
-  }
-
-  if ((listenSocket = socket(servinfo->ai_family, servinfo->ai_socktype,
-                             servinfo->ai_protocol)) == -1) {
-    perror("socket:");
-    exit(EXIT_FAILURE);
-  }
-
-  if (bind(listenSocket, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
-    close(listenSocket);
-    perror("bind:");
-    exit(EXIT_FAILURE);
-  }
-
-  // Libération de la mémoire occupée par les enregistrements
-  freeaddrinfo(servinfo);
-
-  // Attente des requêtes des clients.
-  // Appel non bloquant et passage en mode passif
-  // Demandes d'ouverture de connexion traitées par accept
-  if (listen(listenSocket, BACKLOG) == -1) {
-    perror("listen");
-    exit(EXIT_FAILURE);
-  }
+  if ((status = getaddrinfo(NULL, listenPort, &hints, &servinfo)) != 0) exit(EXIT_FAILURE);
+	//SOCKET
+  if ((listenSocket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) exit(EXIT_FAILURE);
+	//BIND
+  if (bind(listenSocket, servinfo->ai_addr, servinfo->ai_addrlen) == -1) { close(listenSocket); exit(EXIT_FAILURE);}
+  freeaddrinfo(servinfo); // Libération de la mémoire occupée par les enregistrements
+	//LISTEN
+  if (listen(listenSocket, 5) == -1) { close(listenSocket); exit(EXIT_FAILURE);}
 
   while (1) {
+  	//ACCEPT
     printf("Attente de connexion TCP sur le port %s\n", listenPort);
+    if ((connectSocket = accept(listenSocket, (struct sockaddr *) &clientAddress, &clientAddressLength)) == -1) { close(listenSocket); exit(EXIT_FAILURE);}
+    printf("<<  nouveau client %s", inet_ntoa(clientAddress.sin_addr));
+    printf(":%hu\n", ntohs(clientAddress.sin_port));
 
-    // Appel accept() bloquant
-    // connectSocket est une nouvelle prise indépendante
-    if ((connectSocket = accept(listenSocket, 
-                 (struct sockaddr *) &clientAddress,
-                               &clientAddressLength)) == -1) {
-      perror("accept:");
-      close(listenSocket);
-      exit(EXIT_FAILURE);
-    }
 
-      // Affichage de l'adresse IP du client.
-    // inet_ntoa() convertit une adresse IP stockée sous forme binaire en une
-    // chaîne de caractères
-    printf(">>  connecté à %s", inet_ntoa(clientAddress.sin_addr));
-
-    // Affichage du numéro de port client
-    // ntohs() convertit un entier court (short) de l'agencement réseau (octet
-    // de poids fort en premier) vers l'agencement hôte (sur x86 on trouve
-    // l'octet de poids faible en premier).
-      printf(":%hu\n", ntohs(clientAddress.sin_port));
-
-    // Lecture de la chaîne sur le socket en utilisant recv(). La chaîne est
-    // stockée dans le tableau msg. Si aucun message n'arrive, recv() reste en
-    // attente.
-    // On remplit le tableau avec des zéros de façon à connaître la fin de
-    // chaîne de caractères
     memset(msg, 0, sizeof msg);
     while (recv(connectSocket, msg, sizeof msg, 0) > 0) {
       msgLength = strlen(msg);
@@ -112,5 +62,18 @@ int main()
         memset(msg, 0, sizeof msg);  // Mise à zéro du tampon
       }
     }
+    
+/*		//RECEIVE*/
+/*    memset(msg, 0, sizeof msg);*/
+/*    */
+/*    while (recv(connectSocket, msg, sizeof msg, 0) > 0) {*/
+/*    	msgLength = strlen(msg);*/
+/*    	printf("%d",msgLength);*/
+/*     	if (msgLength > 0) {*/
+/*        printf("  --  %s\n", msg);*/
+/*      	if (send(connectSocket, msg, msgLength + 1, 0) == -1) { close(listenSocket); exit(EXIT_FAILURE);}*/
+/*      	memset(msg, 0, sizeof msg);  // Mise à zéro du tampon*/
+/*    	}*/
+/*    }*/
   }
 }
--- C/Liste/c1/c_udp/udp_talker.c
+++ C/Liste/c1/c_udp/udp_talker.c
@@ -1,101 +1,53 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <netdb.h>
-#include <netinet/in.h>
+#include <unistd.h> //close
+#include <stdio.h>	//printf
+#include <stdlib.h> //exit
+#include <string.h> //str
+#include <netdb.h>	//network
 
-#define MAX_PORT 5
-#define PORT_ARRAY_SIZE (MAX_PORT+1)
-#define MAX_MSG 80
-#define MSG_ARRAY_SIZE (MAX_MSG+1)
-// Utilisation d'une constante x dans la définition
-// du format de saisie
 #define str(x) # x
-#define xstr(x) str(x)
 
-int main()
-{
-  int socketDescriptor, status;
-  unsigned int msgLength;
-  struct addrinfo hints, *servinfo;
-  char msg[MSG_ARRAY_SIZE], serverPort[PORT_ARRAY_SIZE];
+int main(){
+	int listenSocket, status;
+	unsigned int msgLength;
+	struct addrinfo hints, *servinfo;
+	char msg[81], serverPort[6];
 
-  puts("Entrez le nom du serveur ou son adresse IP : ");
-  memset(msg, 0, sizeof msg);  // Mise à zéro du tampon
-  scanf("%"xstr(MAX_MSG)"s", msg);
+	// IP address
+	puts("IP Serveur: ");
+	memset(msg, 0, sizeof msg);	// Mise à zéro du tampon
+	scanf("%"str(80)"s", msg);
 
-  puts("Entrez le numéro de port du serveur : ");
-  memset(serverPort, 0, sizeof serverPort);  // Mise à zéro du tampon
-  scanf("%"xstr(MAX_PORT)"s", serverPort);
+	// Port address
+	puts("Port Serveur: ");
+	memset(serverPort, 0, sizeof serverPort);	// Mise à zéro du tampon
+	scanf("%"str(5)"s", serverPort);
 
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
 
-  if ((status = getaddrinfo(msg, serverPort, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-    exit(EXIT_FAILURE);
-  }
+	if ((status = getaddrinfo(msg, serverPort, &hints, &servinfo)) != 0) exit(EXIT_FAILURE);
+	//SOCKET
+	if ((listenSocket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) exit(EXIT_FAILURE);
+	//CONNECT
+	if (connect(listenSocket, servinfo->ai_addr, servinfo->ai_addrlen) == -1) exit(EXIT_FAILURE);
+	puts("Connecté ");
+	freeaddrinfo(servinfo);
 
-  if ((socketDescriptor = socket(servinfo->ai_family, servinfo->ai_socktype,
-                                 servinfo->ai_protocol)) == -1) {
-    perror("socket:");
-    exit(EXIT_FAILURE);
-  }
+	while (strcmp(msg, ".")) {
+		// SEND
+		memset(msg, 0, sizeof msg);	// Mise à zéro du tampon
+		scanf(" %"str(80)"[^\n]%*c", msg);
+		if (sendto(listenSocket, msg, msgLength, 0, servinfo->ai_addr, servinfo->ai_addrlen) == -1) exit(EXIT_FAILURE);
 
-  if (connect(socketDescriptor, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
-    close(socketDescriptor);
-    perror("connect");
-    exit(EXIT_FAILURE);
-  }
+		//RECEIVE
+		memset(msg, 0, sizeof msg);
+		if (recv(listenSocket, msg, sizeof msg, 0) == -1) exit(EXIT_FAILURE);
+		printf("<< : %s\n", msg);
+	}
 
-  // Libération de la mémoire occupée par les enregistrements
-  freeaddrinfo(servinfo);
+	//CLOSE
+	close(listenSocket);
 
-  puts("\nEntrez quelques caractères au clavier.");
-  puts("Le serveur les modifiera et les renverra.");
-  puts("Pour sortir, entrez une ligne avec le caractère '.' uniquement.");
-  puts("Si une ligne dépasse "xstr(MAX_MSG)" caractères,");
-  puts("seuls les "xstr(MAX_MSG)" premiers caractères seront utilisés.\n");
-
-  // Invite de commande pour l'utilisateur et lecture des caractères jusqu'à la
-  // limite MAX_MSG. Puis suppression du saut de ligne en mémoire tampon.
-  puts("Saisie du message : ");
-  memset(msg, 0, sizeof msg);  // Mise à zéro du tampon
-  scanf(" %"xstr(MAX_MSG)"[^\n]%*c", msg);
-
-  // Arrêt lorsque l'utilisateur saisit une ligne ne contenant qu'un point
-  while (strcmp(msg, ".")) {
-    if ((msgLength = strlen(msg)) > 0) {
-      // Envoi de la ligne au serveur
-      if (sendto(socketDescriptor, msg, msgLength, 0,
-                 servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
-        perror("sendto:");
-        close(socketDescriptor);
-        exit(EXIT_FAILURE);
-      }
-
-        // Lecture de la ligne modifiée par le serveur.
-        memset(msg, 0, sizeof msg);  // Mise à zéro du tampon
-        if (recv(socketDescriptor, msg, sizeof msg, 0) == -1) {
-          perror("recv:");
-          close(socketDescriptor);
-          exit(EXIT_FAILURE);
-        }
-    }
-
-        printf("Message traité : %s\n", msg);
-
-    // Invite de commande pour l'utilisateur et lecture des caractères jusqu'à la
-    // limite MAX_MSG. Puis suppression du saut de ligne en mémoire tampon.
-    // Comme ci-dessus.
-    puts("Saisie du message : ");
-    memset(msg, 0, sizeof msg);  // Mise à zéro du tampon
-    scanf(" %"xstr(MAX_MSG)"[^\n]%*c", msg);
-  }
-
-  close(socketDescriptor);
-
-  return 0;
+	return 0;
 }
